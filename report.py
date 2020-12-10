527de33c12a6e87524353961338b008ed05dd5ca
JaviCanollo <javierjf4@gmail.com>
2020-12-10 23:01:16 +0100

    prints para debug

--- report.py
+++ report.py
@@ -1,6 +1,7 @@
 from collections import Counter
-
+print('1')
 with open('/var/log/nginx/access.log', 'r') as f:
+    print('2')
     ip_list = []
     for line in f.readlines():
         ip = ''
@@ -12,8 +13,9 @@ with open('/var/log/nginx/access.log', 'r') as f:
                 break
         ip_counter = Counter(ip_list)
     f.close()
-
+print('3')
 with open('./report.txt', 'w') as f:
+    print('4')
     for ip in ip_counter:
         f.write(f'{ip} --> {ip_counter[ip]}\n')
     f.close()
